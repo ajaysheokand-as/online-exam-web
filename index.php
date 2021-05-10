fdef746cf2b64846abf5257e44cac64f5a05adc2
Abhishek <21jiabhi@gmail.com>
2021-05-10 09:21:54 +0530

    right ans pass with question id on next page " hi asdfjkl"

--- index.php
+++ index.php
@@ -35,14 +35,47 @@
     </div>
     </nav>
     <br>
-
+<form action=""> 
+<!-- Question show on screen -->
+<div class="container">
+    
+    <div class="card text-left">
     <?php
 session_start();
 include('config.php');
+if(isset($_GET['ques_ids'])){
+    $ques_id_str = $_GET['ques_ids'];
+    $ques_id_array = explode(',',$ques_id_str);
+    for($i=0;$i<(count($ques_id_array)-1);$i++){
+        echo 'qus ',$i+1,': ';
+        if(!isset($_GET['ques_selected_option'.($i+1)]))
+         $selected_ans = 0;
+         else {
+        $selected_ans = $_GET['ques_selected_option'.($i+1)];
+        
+        $sql = "SELECT * FROM question where qid=$ques_id_array[$i]";
+        $res = mysqli_query($conn,$sql);
+        while($row = mysqli_fetch_assoc($res)){
+            $db_rigth_ans =  $row['ans'];
+        }
+    }
+        if($selected_ans ==0) echo 'not selected any option<br>';
+    
+    else{
+        if($selected_ans == $db_rigth_ans){
+            echo 'right ans <br>';
+        }else echo 'wrong ans <br>';
+    }
+}
+}
+else{
+$que_id = "";
+
 for($i=1; $i<=10;$i++){
 
 
 $qid=rand(1,886);
+$que_id .= $qid.",";
 $sql = "SELECT ques FROM question where qid=$qid";
 $result = $conn->query($sql);
 
@@ -68,16 +101,13 @@ $topic = $conn->query($sql);
 
 ?>
 
-<!-- Question show on screen -->
-    <div class="contaniner">
-    <!-- <form > -->
-        <div class="card text-left">
+
             <div class="card-header">
                <h6>
                 <?php 
                 
                 while( $row = mysqli_fetch_assoc($result) ){
-                    echo "Question: ", $row['ques'];
+                    echo "Question ".$i.": ", $row['ques'];
                 } ?>
                 
             </div>
@@ -85,8 +115,8 @@ $topic = $conn->query($sql);
            
             <div class="card-body">
             <div class="form-check">
-                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-                    <label class="form-check-label" for="flexRadioDefault1" >1.
+                    <input class="form-check-input" type="radio" value="1" name="ques_selected_option<?php echo $i; ?>" id="flexRadioDefault1<?php echo $i; ?>">
+                    <label class="form-check-label" for="flexRadioDefault1<?php echo $i; ?>" >1.
                     <?php 
                 
                 while( $row = mysqli_fetch_assoc($a) ){
@@ -95,8 +125,8 @@ $topic = $conn->query($sql);
                     </label>
                     </div>
                     <div class="form-check">
-                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" >
-                    <label class="form-check-label" for="flexRadioDefault2">2.
+                    <input class="form-check-input" type="radio" value="2" name="ques_selected_option<?php echo $i; ?>" id="flexRadioDefault2<?php echo $i; ?>" >
+                    <label class="form-check-label" for="flexRadioDefault2<?php echo $i; ?>">2.
                     <?php 
                 
                 while( $row = mysqli_fetch_assoc($b) ){
@@ -105,8 +135,8 @@ $topic = $conn->query($sql);
                     </label>
                     </div>
                     <div class="form-check">
-                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-                    <label class="form-check-label" for="flexRadioDefault3">3.
+                    <input class="form-check-input" type="radio" value="3" name="ques_selected_option<?php echo $i; ?>" id="flexRadioDefault3<?php echo $i; ?>">
+                    <label class="form-check-label" for="flexRadioDefault3<?php echo $i; ?>">3.
                     <?php 
                 
                 while( $row = mysqli_fetch_assoc($c) ){
@@ -115,10 +145,9 @@ $topic = $conn->query($sql);
                     </label>
                     </div>
                     <div class="form-check">
-                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" >
-                    <label class="form-check-label" for="flexRadioDefault4" >4.
+                    <input class="form-check-input" value="4" type="radio" name="ques_selected_option<?php echo $i; ?>" id="flexRadioDefault4<?php echo $i; ?>" >
+                    <label class="form-check-label" for="flexRadioDefault4<?php echo $i; ?>" >4.
                     <?php 
-                // ghfg
                 while( $row = mysqli_fetch_assoc($d) ){
                     echo $row['d'];
                 } ?>
@@ -130,22 +159,19 @@ $topic = $conn->query($sql);
                     echo "Answer is: ".$row['ans'];
                 } ?>
                  </label>
-                   
                     </div>
-            
                  </lable>
                  </br>
                  <hr>
-                <a href="index.php">
+                <!-- <a href="index.php">
                 <button type="submit" id="submit" name="submit" class="btn btn-success"> Next </button>
                 </a>
                 
                 <button type="reset" id="reset" name="reset" class="btn btn-primary"> Reset </button>
                 
-                <button id="showans" name="showans" class="btn btn-dark" onclick="showAns(<?php echo $i; ?>);"> Show Answer </button>
+                <button id="showans" name="showans" class="btn btn-dark" onclick="showAns(<?php echo $i; ?>);"> Show Answer </button> -->
                 </br>
                 </br>
-                <h1>Abhishek Singh Dhull</h1>
                 <lable id="topic" name="topic" value=" "  > Topic: 
                 <?php 
                     while( $row = mysqli_fetch_assoc($topic) ){
@@ -153,7 +179,10 @@ $topic = $conn->query($sql);
                 } ?>
                  </lable>
             </div>
-            <!-- </form> -->
+            <?php } ?><input type="text" name="ques_ids" value="<?php  print_r($que_id); ?> " />
+
+            <input type="submit" value="Submit" name="submit" class ="btn btn-success">
+            </form>
             <?php } ?>
             <div class="card-footer text-muted">
                 Created By : AjAy SHEOKAND
@@ -185,9 +214,9 @@ $topic = $conn->query($sql);
 
     </script>
     <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
+    
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
-    -->
+   
   </body>
 </html>
